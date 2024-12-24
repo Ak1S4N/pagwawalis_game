c86739e3c8857a75c8d470ff68cb007ad7fab5bf
Ak1S4N <107114197+Ak1S4N@users.noreply.github.com>
2024-12-24 11:47:08 +0800

    24/12

--- source/actors/enemies/trash_boss/trash_boss.gd
+++ source/actors/enemies/trash_boss/trash_boss.gd
@@ -10,6 +10,9 @@ enum states {
 var current_state = states.IDLE
 var player_in: PlayerFighting
 
+@onready var timer: Timer = $Timer
+
+
 func slash_attack() -> void:
 	if player_in:
 		print("attacked")
@@ -17,10 +20,24 @@ func slash_attack() -> void:
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.get_class() == 'PlayerFighting':
+	if body.is_in_group('fighter'):
 		player_in = body
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	if body.get_class() == "PlayerFighting":
+	if body.is_in_group('fighter'):
 		player_in = null
+
+
+func _on_timer_timeout() -> void:
+	var rand_i = randi_range(0, 10)
+	if rand_i >= 7:
+		current_state = states.SCREAM
+	else:
+		current_state = states.SLASH
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if (anim_name == "slash") or (anim_name == "scream"):
+		timer.start()
+		current_state = states.IDLE
