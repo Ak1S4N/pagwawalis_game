e5ae719ae5906df1949b8b8fa2d6cc4aa26cd7f3
Ak1S4N <107114197+Ak1S4N@users.noreply.github.com>
2025-01-21 21:48:33 +0800

    21/01

--- source/UI/segregation_ui/segregation.gd
+++ source/UI/segregation_ui/segregation.gd
@@ -9,3 +9,9 @@ func _ready() -> void:
 	for i in trash_number:
 		var trash_instance = TRASH.instantiate()
 		trash_container.add_child(trash_instance)
+		trash_instance.gui_input.connect(trash_gui_event)
+
+func trash_gui_event(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			print("cool")
--- source/UI/segregation_ui/trash/trash_container.gd
+++ source/UI/segregation_ui/trash/trash_container.gd
@@ -6,3 +6,7 @@ func _on_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			print("cool")
+
+
+func _on_mouse_entered() -> void:
+	print("uncool")
